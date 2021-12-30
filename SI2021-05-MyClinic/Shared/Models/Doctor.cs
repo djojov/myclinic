207f61a089027e6e8553d157c1f3f46f9e96e506
Uroš Milošević <milosevicurosit@gmail.com>
2021-12-30 22:18:15 +0100

    Add Model Doctor class

---
+++ SI2021-05-MyClinic/Shared/Models/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    class Doctor
+    {
+        public int Id { get; set; }
+        public bool Status{ get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonalNumber { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DateEmployed { get; set; }
+        public string Specialization { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
