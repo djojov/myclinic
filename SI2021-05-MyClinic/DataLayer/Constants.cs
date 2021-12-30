2832d9c5f25884cacb2bef2f059799e11a9583ce
Uroš Milošević <milosevicurosit@gmail.com>
2021-12-30 22:44:17 +0100

    Add Constant class

---
+++ SI2021-05-MyClinic/DataLayer/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class Constants
+    {
+        public static string ConnectionString = "Data Source=DESKTOP-74OJ5FG\\MYDB;Initial Catalog=MyClinic;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
--- SI2021-05-MyClinic/Shared/Models/Doctor.cs
+++ SI2021-05-MyClinic/Shared/Models/Doctor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Shared.Models
 {
-    class Doctor
+    public class Doctor
     {
         public int Id { get; set; }
         public bool Status{ get; set; }
