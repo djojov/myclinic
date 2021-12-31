01873cc0a84aa123940d229af1a01566897266ae
Uroš Milošević <milosevicurosit@gmail.com>
2021-12-31 01:01:21 +0100

    Add IDoctorRepository interface

---
+++ SI2021-05-MyClinic/Shared/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    interface IDoctorRepository
+    {
+        List<Patient> GetAllPatients();
+        int UpdatePatient(Patient patient);
+        int InsertPatient(Patient patient);
+    }
+}
