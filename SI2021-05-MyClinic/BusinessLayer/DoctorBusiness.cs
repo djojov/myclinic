5cf63b90de723bf85fae241cb75ed997addfb543
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-07 00:58:36 +0100

    Add doctor business class and interface

---
+++ SI2021-05-MyClinic/BusinessLayer/DoctorBusiness.cs
@@ -0,0 +1,51 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class DoctorBusiness : IDoctorBusiness
+    {
+        private readonly IDoctorRepository doctorRepository;
+
+        public DoctorBusiness(IDoctorRepository _doctorRepository)
+        {
+            doctorRepository = _doctorRepository;
+        }
+
+        public List<Patient> GetAllPatients()
+        {
+            return this.doctorRepository.GetAllPatients();
+        }
+        public string InsertPatient(Patient patient)
+        {
+            int rowsAffected = this.doctorRepository.InsertPatient(patient);
+
+            if (rowsAffected > 0)
+            {
+                return "Uspešno ste uneli pacijenta u bazu!";
+            }
+            else
+            {
+                return "Neuspešan unos pacijenta, došlo je do greške!";
+            }
+        }
+        public string UpdatePatient(Patient patient)
+        {
+            int rowsAffected = this.doctorRepository.UpdatePatient(patient);
+
+            if (rowsAffected > 0)
+            {
+                return "Podaci pacijenta su uspešno ažurirani!";
+            }
+            else
+            {
+                return "Neuspešno ažuriranje pacijenta, došlo je do greške!";
+            }
+        }
+    }
+}
---
+++ SI2021-05-MyClinic/Shared/Interfaces/IDoctorBusiness.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    public interface IDoctorBusiness
+    {
+        List<Patient> GetAllPatients();
+        string UpdatePatient(Patient patient);
+        string InsertPatient(Patient patient);
+    }
+}
