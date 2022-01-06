ab91a800086ac01ee1169054bc17a6d472816d8b
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-07 00:23:13 +0100

    Add admin business class and interface.

---
+++ SI2021-05-MyClinic/BusinessLayer/AdminBusiness.cs
@@ -0,0 +1,51 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class AdminBusiness : IAdminBusiness
+    {
+        private readonly IAdminRepository adminRepository;
+
+        public AdminBusiness(IAdminRepository _adminRepository)
+        {
+            adminRepository = _adminRepository;
+        }
+
+        public List<Doctor> GetAllDoctors()
+        {
+            return this.adminRepository.GetAllDoctors();
+        }
+        public string InsertDoctor(Doctor doctor)
+        {
+            int rowsAffected = this.adminRepository.InsertDoctor(doctor);
+
+            if (rowsAffected > 0)
+            {
+                return "Uspešno ste uneli doktora u bazu!";
+            }
+            else
+            {
+                return "Neuspešan unos doktora, došlo je do greške!";
+            }
+        }
+        public string UpdateDoctor(Doctor doctor)
+        {
+            int rowsAffected = this.adminRepository.UpdateDoctor(doctor);
+
+            if (rowsAffected > 0)
+            {
+                return "Podaci doktora su uspešno ažurirani!";
+            }
+            else
+            {
+                return "Neuspešno ažuriranje doktora, došlo je do greške!";
+            }
+        }
+    }
+}
