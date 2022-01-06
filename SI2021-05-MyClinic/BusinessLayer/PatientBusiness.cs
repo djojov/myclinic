1dcf10c484c00ee41f90f435f2d9080158182dd9
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-07 00:37:09 +0100

    Add patient class and interface.

---
+++ SI2021-05-MyClinic/BusinessLayer/PatientBusiness.cs
@@ -0,0 +1,39 @@
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class PatientBusiness : IPatientBusiness
+    {
+        private readonly IPatientRepository patientRepository;
+
+        public PatientBusiness(IPatientRepository _patientRepository)
+        {
+            patientRepository = _patientRepository;
+        }
+
+        public List<string> GetReportData(int id)
+        {
+            return this.patientRepository.GetReportData(id);
+        }
+
+        public string UpdateSelf(Patient patient)
+        {
+            int rowsAffected = this.patientRepository.UpdateSelf(patient);
+
+            if (rowsAffected > 0)
+            {
+                return "Podaci su uspešno ažurirani!";
+            }
+            else
+            {
+                return "Neuspešno ažuriranje, došlo je do greške!";
+            }
+        }
+    }
+}
---
+++ SI2021-05-MyClinic/Shared/Interfaces/IPatientBusiness.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    public interface IPatientBusiness
+    {
+        List<string> GetReportData(int id);
+        string UpdateSelf(Patient patient);
+    }
+}
