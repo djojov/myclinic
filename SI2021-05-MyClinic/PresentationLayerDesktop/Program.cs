61eb16a9c0c51d401fc372484c0232c4e9b8dab0
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2022-01-09 19:59:33 +0100

    Add dependency injection

--- SI2021-05-MyClinic/PresentationLayerDesktop/Program.cs
+++ SI2021-05-MyClinic/PresentationLayerDesktop/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataLayer;
+using DataLayer.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +21,26 @@ namespace PresentationLayerDesktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var login = serviceProvider.GetRequiredService<LoginForm>();
+                Application.Run(login);
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IAdminBusiness, AdminBusiness>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IDoctorBusiness, DoctorBusiness>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IPatientBusiness, PatientBusiness>();
+            services.AddScoped<LoginForm>();
         }
     }
+    
 }
