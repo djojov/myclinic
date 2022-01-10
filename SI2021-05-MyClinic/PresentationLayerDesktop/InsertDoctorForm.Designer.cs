0e4cebd8b2305746ee94639e146d98d13c4ce55f
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2022-01-10 17:29:52 +0100

    Add Insert Doctor Form

---
+++ SI2021-05-MyClinic/PresentationLayerDesktop/InsertDoctorForm.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace PresentationLayerDesktop
+{
+    partial class InsertDoctorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.label_LastName = new System.Windows.Forms.Label();
+            this.label_PersonalNumber = new System.Windows.Forms.Label();
+            this.label_PhoneNumber = new System.Windows.Forms.Label();
+            this.label_DateEmployed = new System.Windows.Forms.Label();
+            this.label_Specialization = new System.Windows.Forms.Label();
+            this.label_Department = new System.Windows.Forms.Label();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_PersonalNumber = new System.Windows.Forms.TextBox();
+            this.textBox_PhoneNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Specialization = new System.Windows.Forms.TextBox();
+            this.textBox_Department = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_DateEmployed = new System.Windows.Forms.DateTimePicker();
+            this.button_Insert = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Location = new System.Drawing.Point(12, 28);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(80, 17);
+            this.label_FirstName.TabIndex = 0;
+            this.label_FirstName.Text = "First Name:";
+            // 
+            // label_LastName
+            // 
+            this.label_LastName.AutoSize = true;
+            this.label_LastName.Location = new System.Drawing.Point(12, 57);
+            this.label_LastName.Name = "label_LastName";
+            this.label_LastName.Size = new System.Drawing.Size(80, 17);
+            this.label_LastName.TabIndex = 1;
+            this.label_LastName.Text = "Last Name:";
+            // 
+            // label_PersonalNumber
+            // 
+            this.label_PersonalNumber.AutoSize = true;
+            this.label_PersonalNumber.Location = new System.Drawing.Point(12, 83);
+            this.label_PersonalNumber.Name = "label_PersonalNumber";
+            this.label_PersonalNumber.Size = new System.Drawing.Size(122, 17);
+            this.label_PersonalNumber.TabIndex = 2;
+            this.label_PersonalNumber.Text = "Personal Number:";
+            // 
+            // label_PhoneNumber
+            // 
+            this.label_PhoneNumber.AutoSize = true;
+            this.label_PhoneNumber.Location = new System.Drawing.Point(13, 110);
+            this.label_PhoneNumber.Name = "label_PhoneNumber";
+            this.label_PhoneNumber.Size = new System.Drawing.Size(107, 17);
+            this.label_PhoneNumber.TabIndex = 3;
+            this.label_PhoneNumber.Text = "Phone Number:";
+            // 
+            // label_DateEmployed
+            // 
+            this.label_DateEmployed.AutoSize = true;
+            this.label_DateEmployed.Location = new System.Drawing.Point(12, 137);
+            this.label_DateEmployed.Name = "label_DateEmployed";
+            this.label_DateEmployed.Size = new System.Drawing.Size(108, 17);
+            this.label_DateEmployed.TabIndex = 4;
+            this.label_DateEmployed.Text = "Date Employed:";
+            // 
+            // label_Specialization
+            // 
+            this.label_Specialization.AutoSize = true;
+            this.label_Specialization.Location = new System.Drawing.Point(12, 165);
+            this.label_Specialization.Name = "label_Specialization";
+            this.label_Specialization.Size = new System.Drawing.Size(99, 17);
+            this.label_Specialization.TabIndex = 5;
+            this.label_Specialization.Text = "Specialization:";
+            // 
+            // label_Department
+            // 
+            this.label_Department.AutoSize = true;
+            this.label_Department.Location = new System.Drawing.Point(12, 192);
+            this.label_Department.Name = "label_Department";
+            this.label_Department.Size = new System.Drawing.Size(86, 17);
+            this.label_Department.TabIndex = 6;
+            this.label_Department.Text = "Department:";
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Location = new System.Drawing.Point(12, 218);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(51, 17);
+            this.label_Email.TabIndex = 7;
+            this.label_Email.Text = "E-mail:";
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(12, 244);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(73, 17);
+            this.label_Password.TabIndex = 8;
+            this.label_Password.Text = "Password:";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(178, 23);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(200, 22);
+            this.textBox_FirstName.TabIndex = 10;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(178, 51);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(200, 22);
+            this.textBox_LastName.TabIndex = 11;
+            // 
+            // textBox_PersonalNumber
+            // 
+            this.textBox_PersonalNumber.Location = new System.Drawing.Point(178, 78);
+            this.textBox_PersonalNumber.Name = "textBox_PersonalNumber";
+            this.textBox_PersonalNumber.Size = new System.Drawing.Size(200, 22);
+            this.textBox_PersonalNumber.TabIndex = 12;
+            // 
+            // textBox_PhoneNumber
+            // 
+            this.textBox_PhoneNumber.Location = new System.Drawing.Point(178, 105);
+            this.textBox_PhoneNumber.Name = "textBox_PhoneNumber";
+            this.textBox_PhoneNumber.Size = new System.Drawing.Size(200, 22);
+            this.textBox_PhoneNumber.TabIndex = 13;
+            // 
+            // textBox_Specialization
+            // 
+            this.textBox_Specialization.Location = new System.Drawing.Point(178, 160);
+            this.textBox_Specialization.Name = "textBox_Specialization";
+            this.textBox_Specialization.Size = new System.Drawing.Size(200, 22);
+            this.textBox_Specialization.TabIndex = 14;
+            // 
+            // textBox_Department
+            // 
+            this.textBox_Department.Location = new System.Drawing.Point(178, 187);
+            this.textBox_Department.Name = "textBox_Department";
+            this.textBox_Department.Size = new System.Drawing.Size(200, 22);
+            this.textBox_Department.TabIndex = 15;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Location = new System.Drawing.Point(178, 213);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(200, 22);
+            this.textBox_Email.TabIndex = 16;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(178, 239);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.Size = new System.Drawing.Size(200, 22);
+            this.textBox_Password.TabIndex = 17;
+            // 
+            // dateTimePicker_DateEmployed
+            // 
+            this.dateTimePicker_DateEmployed.Location = new System.Drawing.Point(178, 133);
+            this.dateTimePicker_DateEmployed.Name = "dateTimePicker_DateEmployed";
+            this.dateTimePicker_DateEmployed.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker_DateEmployed.TabIndex = 18;
+            // 
+            // button_Insert
+            // 
+            this.button_Insert.Location = new System.Drawing.Point(53, 334);
+            this.button_Insert.Name = "button_Insert";
+            this.button_Insert.Size = new System.Drawing.Size(95, 41);
+            this.button_Insert.TabIndex = 19;
+            this.button_Insert.Text = "Insert";
+            this.button_Insert.UseVisualStyleBackColor = true;
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(255, 334);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(86, 41);
+            this.button_Close.TabIndex = 20;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            // 
+            // InsertDoctorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 412);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_Insert);
+            this.Controls.Add(this.dateTimePicker_DateEmployed);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.textBox_Department);
+            this.Controls.Add(this.textBox_Specialization);
+            this.Controls.Add(this.textBox_PhoneNumber);
+            this.Controls.Add(this.textBox_PersonalNumber);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.textBox_FirstName);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.label_Department);
+            this.Controls.Add(this.label_Specialization);
+            this.Controls.Add(this.label_DateEmployed);
+            this.Controls.Add(this.label_PhoneNumber);
+            this.Controls.Add(this.label_PersonalNumber);
+            this.Controls.Add(this.label_LastName);
+            this.Controls.Add(this.label_FirstName);
+            this.Name = "InsertDoctorForm";
+            this.Text = "Insert Doctor";
+            this.Load += new System.EventHandler(this.InsertDoctorForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_FirstName;
+        private System.Windows.Forms.Label label_LastName;
+        private System.Windows.Forms.Label label_PersonalNumber;
+        private System.Windows.Forms.Label label_PhoneNumber;
+        private System.Windows.Forms.Label label_DateEmployed;
+        private System.Windows.Forms.Label label_Specialization;
+        private System.Windows.Forms.Label label_Department;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_PersonalNumber;
+        private System.Windows.Forms.TextBox textBox_PhoneNumber;
+        private System.Windows.Forms.TextBox textBox_Specialization;
+        private System.Windows.Forms.TextBox textBox_Department;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_DateEmployed;
+        private System.Windows.Forms.Button button_Insert;
+        private System.Windows.Forms.Button button_Close;
+    }
+}
---
+++ SI2021-05-MyClinic/PresentationLayerDesktop/InsertDoctorForm.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayerDesktop
+{
+    public partial class InsertDoctorForm : Form
+    {
+        private readonly IAdminBusiness adminBusiness;
+        public InsertDoctorForm(IAdminBusiness _adminBusiness)
+        {
+            adminBusiness = _adminBusiness;
+            InitializeComponent();
+        }
+
+        private void InsertDoctorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
