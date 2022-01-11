f23e1d0a8897983d2e1755e30602888020b7c651
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2022-01-11 22:55:25 +0100

    Add funcionality to Patient Form

--- SI2021-05-MyClinic/PresentationLayerDesktop/PatientForm.Designer.cs
+++ SI2021-05-MyClinic/PresentationLayerDesktop/PatientForm.Designer.cs
@@ -29,19 +29,369 @@ namespace PresentationLayerDesktop
         /// </summary>
         private void InitializeComponent()
         {
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.label_LastName = new System.Windows.Forms.Label();
+            this.label_PersonalNumber = new System.Windows.Forms.Label();
+            this.label_HealthInsuranceNumber = new System.Windows.Forms.Label();
+            this.label_DateOfBirth = new System.Windows.Forms.Label();
+            this.label_PlaceOfBirth = new System.Windows.Forms.Label();
+            this.label_PhoneNumber = new System.Windows.Forms.Label();
+            this.label_Weight = new System.Windows.Forms.Label();
+            this.label_Height = new System.Windows.Forms.Label();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.label_PasswordConfirm = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_PersonalNumber = new System.Windows.Forms.TextBox();
+            this.textBox_HealthInsuranceNumber = new System.Windows.Forms.TextBox();
+            this.textBox_DateOfBirth = new System.Windows.Forms.TextBox();
+            this.textBox_PlaceOfBirth = new System.Windows.Forms.TextBox();
+            this.textBox_PhoneNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Weight = new System.Windows.Forms.TextBox();
+            this.textBox_Height = new System.Windows.Forms.TextBox();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.textBox_Password = new System.Windows.Forms.TextBox();
+            this.textBox_NewPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_UpdateData = new System.Windows.Forms.Button();
+            this.button_ReportView = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Location = new System.Drawing.Point(9, 59);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(80, 17);
+            this.label_FirstName.TabIndex = 0;
+            this.label_FirstName.Text = "First Name:";
+            // 
+            // label_LastName
+            // 
+            this.label_LastName.AutoSize = true;
+            this.label_LastName.Location = new System.Drawing.Point(9, 87);
+            this.label_LastName.Name = "label_LastName";
+            this.label_LastName.Size = new System.Drawing.Size(80, 17);
+            this.label_LastName.TabIndex = 1;
+            this.label_LastName.Text = "Last Name:";
+            // 
+            // label_PersonalNumber
+            // 
+            this.label_PersonalNumber.AutoSize = true;
+            this.label_PersonalNumber.Location = new System.Drawing.Point(9, 115);
+            this.label_PersonalNumber.Name = "label_PersonalNumber";
+            this.label_PersonalNumber.Size = new System.Drawing.Size(122, 17);
+            this.label_PersonalNumber.TabIndex = 2;
+            this.label_PersonalNumber.Text = "Personal Number:";
+            // 
+            // label_HealthInsuranceNumber
+            // 
+            this.label_HealthInsuranceNumber.AutoSize = true;
+            this.label_HealthInsuranceNumber.Location = new System.Drawing.Point(9, 143);
+            this.label_HealthInsuranceNumber.Name = "label_HealthInsuranceNumber";
+            this.label_HealthInsuranceNumber.Size = new System.Drawing.Size(173, 17);
+            this.label_HealthInsuranceNumber.TabIndex = 3;
+            this.label_HealthInsuranceNumber.Text = "Health Insurance Number:";
+            // 
+            // label_DateOfBirth
+            // 
+            this.label_DateOfBirth.AutoSize = true;
+            this.label_DateOfBirth.Location = new System.Drawing.Point(9, 171);
+            this.label_DateOfBirth.Name = "label_DateOfBirth";
+            this.label_DateOfBirth.Size = new System.Drawing.Size(91, 17);
+            this.label_DateOfBirth.TabIndex = 4;
+            this.label_DateOfBirth.Text = "Date of Birth:";
+            // 
+            // label_PlaceOfBirth
+            // 
+            this.label_PlaceOfBirth.AutoSize = true;
+            this.label_PlaceOfBirth.Location = new System.Drawing.Point(9, 200);
+            this.label_PlaceOfBirth.Name = "label_PlaceOfBirth";
+            this.label_PlaceOfBirth.Size = new System.Drawing.Size(96, 17);
+            this.label_PlaceOfBirth.TabIndex = 5;
+            this.label_PlaceOfBirth.Text = "Place of Birth:";
+            // 
+            // label_PhoneNumber
+            // 
+            this.label_PhoneNumber.AutoSize = true;
+            this.label_PhoneNumber.Location = new System.Drawing.Point(9, 227);
+            this.label_PhoneNumber.Name = "label_PhoneNumber";
+            this.label_PhoneNumber.Size = new System.Drawing.Size(107, 17);
+            this.label_PhoneNumber.TabIndex = 6;
+            this.label_PhoneNumber.Text = "Phone Number:";
+            // 
+            // label_Weight
+            // 
+            this.label_Weight.AutoSize = true;
+            this.label_Weight.Location = new System.Drawing.Point(9, 257);
+            this.label_Weight.Name = "label_Weight";
+            this.label_Weight.Size = new System.Drawing.Size(56, 17);
+            this.label_Weight.TabIndex = 7;
+            this.label_Weight.Text = "Weight:";
+            // 
+            // label_Height
+            // 
+            this.label_Height.AutoSize = true;
+            this.label_Height.Location = new System.Drawing.Point(9, 286);
+            this.label_Height.Name = "label_Height";
+            this.label_Height.Size = new System.Drawing.Size(53, 17);
+            this.label_Height.TabIndex = 8;
+            this.label_Height.Text = "Height:";
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Location = new System.Drawing.Point(9, 314);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(51, 17);
+            this.label_Email.TabIndex = 9;
+            this.label_Email.Text = "E-mail:";
+            // 
+            // label_Password
+            // 
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(9, 346);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(103, 17);
+            this.label_Password.TabIndex = 10;
+            this.label_Password.Text = "New password:";
+            // 
+            // label_PasswordConfirm
+            // 
+            this.label_PasswordConfirm.AutoSize = true;
+            this.label_PasswordConfirm.Location = new System.Drawing.Point(9, 380);
+            this.label_PasswordConfirm.Name = "label_PasswordConfirm";
+            this.label_PasswordConfirm.Size = new System.Drawing.Size(154, 17);
+            this.label_PasswordConfirm.TabIndex = 11;
+            this.label_PasswordConfirm.Text = "Password confirmation:";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(194, 54);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.ReadOnly = true;
+            this.textBox_FirstName.Size = new System.Drawing.Size(215, 22);
+            this.textBox_FirstName.TabIndex = 12;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(194, 82);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.ReadOnly = true;
+            this.textBox_LastName.Size = new System.Drawing.Size(215, 22);
+            this.textBox_LastName.TabIndex = 13;
+            // 
+            // textBox_PersonalNumber
+            // 
+            this.textBox_PersonalNumber.Location = new System.Drawing.Point(194, 110);
+            this.textBox_PersonalNumber.Name = "textBox_PersonalNumber";
+            this.textBox_PersonalNumber.ReadOnly = true;
+            this.textBox_PersonalNumber.Size = new System.Drawing.Size(215, 22);
+            this.textBox_PersonalNumber.TabIndex = 14;
+            // 
+            // textBox_HealthInsuranceNumber
+            // 
+            this.textBox_HealthInsuranceNumber.Location = new System.Drawing.Point(194, 138);
+            this.textBox_HealthInsuranceNumber.Name = "textBox_HealthInsuranceNumber";
+            this.textBox_HealthInsuranceNumber.ReadOnly = true;
+            this.textBox_HealthInsuranceNumber.Size = new System.Drawing.Size(215, 22);
+            this.textBox_HealthInsuranceNumber.TabIndex = 15;
+            // 
+            // textBox_DateOfBirth
+            // 
+            this.textBox_DateOfBirth.Location = new System.Drawing.Point(194, 166);
+            this.textBox_DateOfBirth.Name = "textBox_DateOfBirth";
+            this.textBox_DateOfBirth.ReadOnly = true;
+            this.textBox_DateOfBirth.Size = new System.Drawing.Size(215, 22);
+            this.textBox_DateOfBirth.TabIndex = 16;
+            // 
+            // textBox_PlaceOfBirth
+            // 
+            this.textBox_PlaceOfBirth.Location = new System.Drawing.Point(194, 194);
+            this.textBox_PlaceOfBirth.Name = "textBox_PlaceOfBirth";
+            this.textBox_PlaceOfBirth.ReadOnly = true;
+            this.textBox_PlaceOfBirth.Size = new System.Drawing.Size(215, 22);
+            this.textBox_PlaceOfBirth.TabIndex = 17;
+            // 
+            // textBox_PhoneNumber
+            // 
+            this.textBox_PhoneNumber.Location = new System.Drawing.Point(194, 222);
+            this.textBox_PhoneNumber.Name = "textBox_PhoneNumber";
+            this.textBox_PhoneNumber.Size = new System.Drawing.Size(215, 22);
+            this.textBox_PhoneNumber.TabIndex = 18;
+            // 
+            // textBox_Weight
+            // 
+            this.textBox_Weight.Location = new System.Drawing.Point(194, 250);
+            this.textBox_Weight.Name = "textBox_Weight";
+            this.textBox_Weight.Size = new System.Drawing.Size(59, 22);
+            this.textBox_Weight.TabIndex = 19;
+            // 
+            // textBox_Height
+            // 
+            this.textBox_Height.Location = new System.Drawing.Point(194, 281);
+            this.textBox_Height.Name = "textBox_Height";
+            this.textBox_Height.Size = new System.Drawing.Size(59, 22);
+            this.textBox_Height.TabIndex = 20;
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Location = new System.Drawing.Point(194, 309);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(215, 22);
+            this.textBox_Email.TabIndex = 21;
+            // 
+            // textBox_Password
+            // 
+            this.textBox_Password.Location = new System.Drawing.Point(194, 341);
+            this.textBox_Password.Name = "textBox_Password";
+            this.textBox_Password.PasswordChar = '*';
+            this.textBox_Password.Size = new System.Drawing.Size(215, 22);
+            this.textBox_Password.TabIndex = 22;
+            // 
+            // textBox_NewPassword
+            // 
+            this.textBox_NewPassword.Location = new System.Drawing.Point(194, 375);
+            this.textBox_NewPassword.Name = "textBox_NewPassword";
+            this.textBox_NewPassword.PasswordChar = '*';
+            this.textBox_NewPassword.Size = new System.Drawing.Size(215, 22);
+            this.textBox_NewPassword.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "kg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "cm";
+            // 
+            // button_UpdateData
+            // 
+            this.button_UpdateData.Location = new System.Drawing.Point(12, 431);
+            this.button_UpdateData.Name = "button_UpdateData";
+            this.button_UpdateData.Size = new System.Drawing.Size(107, 48);
+            this.button_UpdateData.TabIndex = 26;
+            this.button_UpdateData.Text = "Update Data";
+            this.button_UpdateData.UseVisualStyleBackColor = true;
+            this.button_UpdateData.Click += new System.EventHandler(this.button_UpdateData_Click);
+            // 
+            // button_ReportView
+            // 
+            this.button_ReportView.Location = new System.Drawing.Point(158, 431);
+            this.button_ReportView.Name = "button_ReportView";
+            this.button_ReportView.Size = new System.Drawing.Size(107, 48);
+            this.button_ReportView.TabIndex = 27;
+            this.button_ReportView.Text = "View Reports";
+            this.button_ReportView.UseVisualStyleBackColor = true;
+            this.button_ReportView.Click += new System.EventHandler(this.button_ReportView_Click);
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(302, 431);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(107, 48);
+            this.button_Close.TabIndex = 28;
+            this.button_Close.Text = "Log out";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 30);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Calculate BMI";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // PatientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(457, 491);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_ReportView);
+            this.Controls.Add(this.button_UpdateData);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_NewPassword);
+            this.Controls.Add(this.textBox_Password);
+            this.Controls.Add(this.textBox_Email);
+            this.Controls.Add(this.textBox_Height);
+            this.Controls.Add(this.textBox_Weight);
+            this.Controls.Add(this.textBox_PhoneNumber);
+            this.Controls.Add(this.textBox_PlaceOfBirth);
+            this.Controls.Add(this.textBox_DateOfBirth);
+            this.Controls.Add(this.textBox_HealthInsuranceNumber);
+            this.Controls.Add(this.textBox_PersonalNumber);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.textBox_FirstName);
+            this.Controls.Add(this.label_PasswordConfirm);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.label_Email);
+            this.Controls.Add(this.label_Height);
+            this.Controls.Add(this.label_Weight);
+            this.Controls.Add(this.label_PhoneNumber);
+            this.Controls.Add(this.label_PlaceOfBirth);
+            this.Controls.Add(this.label_DateOfBirth);
+            this.Controls.Add(this.label_HealthInsuranceNumber);
+            this.Controls.Add(this.label_PersonalNumber);
+            this.Controls.Add(this.label_LastName);
+            this.Controls.Add(this.label_FirstName);
             this.Name = "PatientForm";
             this.Text = "Patient Panel";
+            this.Load += new System.EventHandler(this.PatientForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_FirstName;
+        private System.Windows.Forms.Label label_LastName;
+        private System.Windows.Forms.Label label_PersonalNumber;
+        private System.Windows.Forms.Label label_HealthInsuranceNumber;
+        private System.Windows.Forms.Label label_DateOfBirth;
+        private System.Windows.Forms.Label label_PlaceOfBirth;
+        private System.Windows.Forms.Label label_PhoneNumber;
+        private System.Windows.Forms.Label label_Weight;
+        private System.Windows.Forms.Label label_Height;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.Label label_PasswordConfirm;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_PersonalNumber;
+        private System.Windows.Forms.TextBox textBox_HealthInsuranceNumber;
+        private System.Windows.Forms.TextBox textBox_DateOfBirth;
+        private System.Windows.Forms.TextBox textBox_PlaceOfBirth;
+        private System.Windows.Forms.TextBox textBox_PhoneNumber;
+        private System.Windows.Forms.TextBox textBox_Weight;
+        private System.Windows.Forms.TextBox textBox_Height;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.TextBox textBox_Password;
+        private System.Windows.Forms.TextBox textBox_NewPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_UpdateData;
+        private System.Windows.Forms.Button button_ReportView;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SI2021-05-MyClinic/PresentationLayerDesktop/PatientForm.cs
+++ SI2021-05-MyClinic/PresentationLayerDesktop/PatientForm.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,106 @@ namespace PresentationLayerDesktop
 {
     public partial class PatientForm : Form
     {
-        public PatientForm()
+        private readonly IAdminBusiness adminBusiness;
+        private readonly IDoctorBusiness doctorBusiness;
+        private readonly IPatientBusiness patientBusiness;
+        Patient patient;
+        public PatientForm(IPatientBusiness _patientBusiness, Patient _patient)
         {
+            patientBusiness = _patientBusiness;
+            patient = _patient;
             InitializeComponent();
+        }
+        public LoginForm RefLoginForm { get; set; }
+        private void PatientForm_Load(object sender, EventArgs e)
+        {
+            textBox_FirstName.Text = patient.FirstName;
+            textBox_LastName.Text = patient.LastName;
+            textBox_PersonalNumber.Text = patient.PersonalNumber;
+            textBox_HealthInsuranceNumber.Text = patient.HealthInsuranceNumber;
+            textBox_DateOfBirth.Text = patient.DateOfBirth.ToString();
+            textBox_PlaceOfBirth.Text = patient.PlaceOfBirth;
+            textBox_PhoneNumber.Text = patient.PhoneNumber;
+            textBox_Weight.Text = patient.Weight;
+            textBox_Height.Text = patient.Height;
+            textBox_Email.Text = patient.Email;
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.RefLoginForm.Show();
+            this.Close();
+        }
+
+        private void button_ReportView_Click(object sender, EventArgs e)
+        {
+            ReportViewForm viewReportForm = new ReportViewForm(patientBusiness, patient, null);
+            viewReportForm.RefPatientForm = this;
+            viewReportForm.Show();
+        }
+
+        private void button_UpdateData_Click(object sender, EventArgs e)
+        {
+            if (textBox_PhoneNumber.Text == "" || textBox_Height.Text == "" || textBox_Weight.Text == "" ||
+                textBox_Email.Text == "")
+            {
+                MessageBox.Show("You have to fill out all of the fields", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_PhoneNumber.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_Email.Text, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$").Success)
+            {
+                MessageBox.Show("E-mail entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_Email.Focus();
+                return;
+            }
+            else if (textBox_Password.Text != "" || textBox_NewPassword.Text != "")
+            {
+                if (!Regex.Match(textBox_Password.Text, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$").Success)
+                {
+                    MessageBox.Show("Password entered incorrectly!\nYour password has to contain at least 8 characters, one uppercase and one lowercase letter and one special character!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox_Password.Text = "";
+                    textBox_NewPassword.Text = "";
+                    textBox_Password.Focus();
+                    return;
+                }
+                else if (textBox_Password.Text != textBox_NewPassword.Text)
+                {
+                    MessageBox.Show("Your confirmation password does not match your password!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox_Password.Text = "";
+                    textBox_NewPassword.Text = "";
+                    textBox_Password.Focus();
+                    return;
+                }
+                else
+                {
+                    patient.Password = patient.Password;
+                }
+            }
+            else if (!Regex.Match(textBox_PhoneNumber.Text, @"^[0][6]\d{1}[1-9]\d{2,3}\d{3,4}$").Success)
+            {
+                MessageBox.Show("Phone number entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_PhoneNumber.Focus();
+                return;
+            }
+            else if (textBox_Password.Text != textBox_NewPassword.Text)
+            {
+                MessageBox.Show("Your confirmation password does not match your password!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_Password.Text = "";
+                textBox_NewPassword.Text = "";
+                textBox_Password.Focus();
+                return;
+            }
+            patient.PhoneNumber = textBox_PhoneNumber.Text;
+            patient.Weight = textBox_Weight.Text;
+            patient.Height = textBox_Height.Text;
+            patient.Email = textBox_Email.Text;
+            if(textBox_Password.Text == "" && textBox_NewPassword.Text == "")
+            {
+                patient.Password = patient.Password;
+            }
+            string result = patientBusiness.UpdateSelf(patient);
+            MessageBox.Show(result, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
