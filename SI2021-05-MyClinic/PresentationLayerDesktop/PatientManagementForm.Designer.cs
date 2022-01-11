84ccbe2c8d1d466a479c6a21ed80e125c34a282e
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2022-01-11 15:32:29 +0100

    Add funcionality to Patient Management Form

--- SI2021-05-MyClinic/PresentationLayerDesktop/PatientManagementForm.Designer.cs
+++ SI2021-05-MyClinic/PresentationLayerDesktop/PatientManagementForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace PresentationLayerDesktop
 {
-    partial class Form1
+    partial class PatientManagementForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,300 @@ namespace PresentationLayerDesktop
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView_PatientList = new System.Windows.Forms.DataGridView();
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.label_LastName = new System.Windows.Forms.Label();
+            this.label_PersonalNumber = new System.Windows.Forms.Label();
+            this.label_HealthInsuranceNumber = new System.Windows.Forms.Label();
+            this.label_DateOfBirth = new System.Windows.Forms.Label();
+            this.label_PlaceOfBirth = new System.Windows.Forms.Label();
+            this.label_PhoneNumber = new System.Windows.Forms.Label();
+            this.label_Weight = new System.Windows.Forms.Label();
+            this.label_Height = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_PersonalNumber = new System.Windows.Forms.TextBox();
+            this.textBox_HealthInsuranceNumber = new System.Windows.Forms.TextBox();
+            this.textBox_PlaceOfBirth = new System.Windows.Forms.TextBox();
+            this.textBox_PhoneNumber = new System.Windows.Forms.TextBox();
+            this.textBox_Weight = new System.Windows.Forms.TextBox();
+            this.textBox_Height = new System.Windows.Forms.TextBox();
+            this.button_UpdatePatient = new System.Windows.Forms.Button();
+            this.button_InsertPatient = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.dateTimePicker_DateOfBirth = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_PatientList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_PatientList
+            // 
+            this.dataGridView_PatientList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_PatientList.Location = new System.Drawing.Point(25, 54);
+            this.dataGridView_PatientList.Name = "dataGridView_PatientList";
+            this.dataGridView_PatientList.RowHeadersWidth = 51;
+            this.dataGridView_PatientList.RowTemplate.Height = 24;
+            this.dataGridView_PatientList.Size = new System.Drawing.Size(1431, 405);
+            this.dataGridView_PatientList.TabIndex = 0;
+            this.dataGridView_PatientList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_PatientList_CellClick);
+            // 
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Location = new System.Drawing.Point(21, 497);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(80, 17);
+            this.label_FirstName.TabIndex = 1;
+            this.label_FirstName.Text = "First Name:";
+            // 
+            // label_LastName
+            // 
+            this.label_LastName.AutoSize = true;
+            this.label_LastName.Location = new System.Drawing.Point(21, 527);
+            this.label_LastName.Name = "label_LastName";
+            this.label_LastName.Size = new System.Drawing.Size(80, 17);
+            this.label_LastName.TabIndex = 2;
+            this.label_LastName.Text = "Last Name:";
+            // 
+            // label_PersonalNumber
+            // 
+            this.label_PersonalNumber.AutoSize = true;
+            this.label_PersonalNumber.Location = new System.Drawing.Point(21, 555);
+            this.label_PersonalNumber.Name = "label_PersonalNumber";
+            this.label_PersonalNumber.Size = new System.Drawing.Size(122, 17);
+            this.label_PersonalNumber.TabIndex = 3;
+            this.label_PersonalNumber.Text = "Personal Number:";
+            // 
+            // label_HealthInsuranceNumber
+            // 
+            this.label_HealthInsuranceNumber.AutoSize = true;
+            this.label_HealthInsuranceNumber.Location = new System.Drawing.Point(21, 583);
+            this.label_HealthInsuranceNumber.Name = "label_HealthInsuranceNumber";
+            this.label_HealthInsuranceNumber.Size = new System.Drawing.Size(173, 17);
+            this.label_HealthInsuranceNumber.TabIndex = 4;
+            this.label_HealthInsuranceNumber.Text = "Health Insurance Number:";
+            // 
+            // label_DateOfBirth
+            // 
+            this.label_DateOfBirth.AutoSize = true;
+            this.label_DateOfBirth.Location = new System.Drawing.Point(21, 612);
+            this.label_DateOfBirth.Name = "label_DateOfBirth";
+            this.label_DateOfBirth.Size = new System.Drawing.Size(91, 17);
+            this.label_DateOfBirth.TabIndex = 5;
+            this.label_DateOfBirth.Text = "Date of Birth:";
+            // 
+            // label_PlaceOfBirth
+            // 
+            this.label_PlaceOfBirth.AutoSize = true;
+            this.label_PlaceOfBirth.Location = new System.Drawing.Point(21, 640);
+            this.label_PlaceOfBirth.Name = "label_PlaceOfBirth";
+            this.label_PlaceOfBirth.Size = new System.Drawing.Size(96, 17);
+            this.label_PlaceOfBirth.TabIndex = 6;
+            this.label_PlaceOfBirth.Text = "Place of Birth:";
+            // 
+            // label_PhoneNumber
+            // 
+            this.label_PhoneNumber.AutoSize = true;
+            this.label_PhoneNumber.Location = new System.Drawing.Point(21, 671);
+            this.label_PhoneNumber.Name = "label_PhoneNumber";
+            this.label_PhoneNumber.Size = new System.Drawing.Size(107, 17);
+            this.label_PhoneNumber.TabIndex = 8;
+            this.label_PhoneNumber.Text = "Phone Number:";
+            // 
+            // label_Weight
+            // 
+            this.label_Weight.AutoSize = true;
+            this.label_Weight.Location = new System.Drawing.Point(21, 700);
+            this.label_Weight.Name = "label_Weight";
+            this.label_Weight.Size = new System.Drawing.Size(56, 17);
+            this.label_Weight.TabIndex = 9;
+            this.label_Weight.Text = "Weight:";
+            // 
+            // label_Height
+            // 
+            this.label_Height.AutoSize = true;
+            this.label_Height.Location = new System.Drawing.Point(21, 725);
+            this.label_Height.Name = "label_Height";
+            this.label_Height.Size = new System.Drawing.Size(53, 17);
+            this.label_Height.TabIndex = 10;
+            this.label_Height.Text = "Height:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 697);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "kg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(345, 725);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "cm";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(242, 494);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(137, 22);
+            this.textBox_FirstName.TabIndex = 13;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(242, 522);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(137, 22);
+            this.textBox_LastName.TabIndex = 14;
+            // 
+            // textBox_PersonalNumber
+            // 
+            this.textBox_PersonalNumber.Location = new System.Drawing.Point(242, 550);
+            this.textBox_PersonalNumber.Name = "textBox_PersonalNumber";
+            this.textBox_PersonalNumber.Size = new System.Drawing.Size(137, 22);
+            this.textBox_PersonalNumber.TabIndex = 15;
+            // 
+            // textBox_HealthInsuranceNumber
+            // 
+            this.textBox_HealthInsuranceNumber.Location = new System.Drawing.Point(242, 578);
+            this.textBox_HealthInsuranceNumber.Name = "textBox_HealthInsuranceNumber";
+            this.textBox_HealthInsuranceNumber.Size = new System.Drawing.Size(137, 22);
+            this.textBox_HealthInsuranceNumber.TabIndex = 16;
+            // 
+            // textBox_PlaceOfBirth
+            // 
+            this.textBox_PlaceOfBirth.Location = new System.Drawing.Point(242, 635);
+            this.textBox_PlaceOfBirth.Name = "textBox_PlaceOfBirth";
+            this.textBox_PlaceOfBirth.Size = new System.Drawing.Size(137, 22);
+            this.textBox_PlaceOfBirth.TabIndex = 17;
+            // 
+            // textBox_PhoneNumber
+            // 
+            this.textBox_PhoneNumber.Location = new System.Drawing.Point(242, 662);
+            this.textBox_PhoneNumber.Name = "textBox_PhoneNumber";
+            this.textBox_PhoneNumber.Size = new System.Drawing.Size(137, 22);
+            this.textBox_PhoneNumber.TabIndex = 19;
+            // 
+            // textBox_Weight
+            // 
+            this.textBox_Weight.Location = new System.Drawing.Point(242, 692);
+            this.textBox_Weight.Name = "textBox_Weight";
+            this.textBox_Weight.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Weight.TabIndex = 20;
+            // 
+            // textBox_Height
+            // 
+            this.textBox_Height.Location = new System.Drawing.Point(242, 720);
+            this.textBox_Height.Name = "textBox_Height";
+            this.textBox_Height.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Height.TabIndex = 21;
+            // 
+            // button_UpdatePatient
+            // 
+            this.button_UpdatePatient.Location = new System.Drawing.Point(603, 555);
+            this.button_UpdatePatient.Name = "button_UpdatePatient";
+            this.button_UpdatePatient.Size = new System.Drawing.Size(130, 50);
+            this.button_UpdatePatient.TabIndex = 23;
+            this.button_UpdatePatient.Text = "Update Patient";
+            this.button_UpdatePatient.UseVisualStyleBackColor = true;
+            this.button_UpdatePatient.Click += new System.EventHandler(this.button_UpdatePatient_Click);
+            // 
+            // button_InsertPatient
+            // 
+            this.button_InsertPatient.Location = new System.Drawing.Point(603, 625);
+            this.button_InsertPatient.Name = "button_InsertPatient";
+            this.button_InsertPatient.Size = new System.Drawing.Size(130, 47);
+            this.button_InsertPatient.TabIndex = 24;
+            this.button_InsertPatient.Text = "Insert New Patient";
+            this.button_InsertPatient.UseVisualStyleBackColor = true;
+            this.button_InsertPatient.Click += new System.EventHandler(this.button_InsertPatient_Click);
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(603, 690);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(130, 36);
+            this.button_Close.TabIndex = 25;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // dateTimePicker_DateOfBirth
+            // 
+            this.dateTimePicker_DateOfBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_DateOfBirth.Location = new System.Drawing.Point(242, 606);
+            this.dateTimePicker_DateOfBirth.Name = "dateTimePicker_DateOfBirth";
+            this.dateTimePicker_DateOfBirth.Size = new System.Drawing.Size(137, 22);
+            this.dateTimePicker_DateOfBirth.TabIndex = 26;
+            // 
+            // PatientManagementForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1468, 751);
+            this.Controls.Add(this.dateTimePicker_DateOfBirth);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_InsertPatient);
+            this.Controls.Add(this.button_UpdatePatient);
+            this.Controls.Add(this.textBox_Height);
+            this.Controls.Add(this.textBox_Weight);
+            this.Controls.Add(this.textBox_PhoneNumber);
+            this.Controls.Add(this.textBox_PlaceOfBirth);
+            this.Controls.Add(this.textBox_HealthInsuranceNumber);
+            this.Controls.Add(this.textBox_PersonalNumber);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.textBox_FirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Height);
+            this.Controls.Add(this.label_Weight);
+            this.Controls.Add(this.label_PhoneNumber);
+            this.Controls.Add(this.label_PlaceOfBirth);
+            this.Controls.Add(this.label_DateOfBirth);
+            this.Controls.Add(this.label_HealthInsuranceNumber);
+            this.Controls.Add(this.label_PersonalNumber);
+            this.Controls.Add(this.label_LastName);
+            this.Controls.Add(this.label_FirstName);
+            this.Controls.Add(this.dataGridView_PatientList);
+            this.Name = "PatientManagementForm";
+            this.Text = "Patient Management";
+            this.Load += new System.EventHandler(this.PatientManagementForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_PatientList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_PatientList;
+        private System.Windows.Forms.Label label_FirstName;
+        private System.Windows.Forms.Label label_LastName;
+        private System.Windows.Forms.Label label_PersonalNumber;
+        private System.Windows.Forms.Label label_HealthInsuranceNumber;
+        private System.Windows.Forms.Label label_DateOfBirth;
+        private System.Windows.Forms.Label label_PlaceOfBirth;
+        private System.Windows.Forms.Label label_PhoneNumber;
+        private System.Windows.Forms.Label label_Weight;
+        private System.Windows.Forms.Label label_Height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_PersonalNumber;
+        private System.Windows.Forms.TextBox textBox_HealthInsuranceNumber;
+        private System.Windows.Forms.TextBox textBox_PlaceOfBirth;
+        private System.Windows.Forms.TextBox textBox_PhoneNumber;
+        private System.Windows.Forms.TextBox textBox_Weight;
+        private System.Windows.Forms.TextBox textBox_Height;
+        private System.Windows.Forms.Button button_UpdatePatient;
+        private System.Windows.Forms.Button button_InsertPatient;
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_DateOfBirth;
     }
 }
--- SI2021-05-MyClinic/PresentationLayerDesktop/PatientManagementForm.cs
+++ SI2021-05-MyClinic/PresentationLayerDesktop/PatientManagementForm.cs
@@ -1,20 +1,146 @@
-﻿using System;
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PresentationLayerDesktop
 {
-    public partial class Form1 : Form
+    public partial class PatientManagementForm : Form
     {
-        public Form1()
+        IDoctorBusiness doctorBusiness;
+        IPatientBusiness patientBusiness;
+        public PatientManagementForm(IDoctorBusiness _doctorBusiness, IPatientBusiness _patientBusiness)
         {
+            doctorBusiness = _doctorBusiness;
+            patientBusiness = _patientBusiness;
             InitializeComponent();
+        }
+        public DoctorForm RefDoctorForm { get; set; }
+
+        private void PatientManagementForm_Load(object sender, EventArgs e)
+        {
+            List<Patient> patientList = doctorBusiness.GetAllPatients();
+            dataGridView_PatientList.DataSource = patientList;
+            dataGridView_PatientList.Columns[7].Visible = false;
+            dataGridView_PatientList.Columns[8].Visible = false;
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            this.RefDoctorForm.Show();
+            this.Close();
+        }
+
+        private void dataGridView_PatientList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+
+                DataGridViewRow row = this.dataGridView_PatientList.Rows[e.RowIndex];
+                textBox_FirstName.Text = row.Cells[1].Value.ToString();
+                textBox_LastName.Text = row.Cells[2].Value.ToString();
+                textBox_PersonalNumber.Text = row.Cells[3].Value.ToString();
+                textBox_HealthInsuranceNumber.Text = row.Cells[4].Value.ToString();
+                dateTimePicker_DateOfBirth.Value = Convert.ToDateTime(row.Cells[5].Value.ToString());
+                textBox_PlaceOfBirth.Text = row.Cells[6].Value.ToString();
+                textBox_PhoneNumber.Text = row.Cells[9].Value.ToString();
+                textBox_Weight.Text = row.Cells[10].Value.ToString();
+                textBox_Height.Text = row.Cells[11].Value.ToString();
+            }
+        }
+
+        private void button_UpdatePatient_Click(object sender, EventArgs e)
+        {
+            if (textBox_FirstName.Text == "" || textBox_LastName.Text == "" || textBox_PersonalNumber.Text == "" ||
+                textBox_PhoneNumber.Text == "" || textBox_HealthInsuranceNumber.Text == "" || textBox_PlaceOfBirth.Text == ""
+                || textBox_Height.Text == "" || textBox_Height.Text == "")
+            {
+                MessageBox.Show("You have to fill out all of the fields", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (!Regex.Match(textBox_FirstName.Text, @"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$").Success)
+            {
+                MessageBox.Show("First name cannot contain a digit!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_FirstName.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_LastName.Text, @"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$").Success)
+            {
+                MessageBox.Show("Last name cannot contain a digit!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_LastName.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_PhoneNumber.Text, @"^[0][6]\d{1}[1-9]\d{2,3}\d{3,4}$").Success)
+            {
+                MessageBox.Show("Phone number entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_PhoneNumber.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_PersonalNumber.Text, @"^(\d{13})?$").Success)
+            {
+                MessageBox.Show("Personal number entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_PersonalNumber.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_HealthInsuranceNumber.Text, @"^(\d{11})?$").Success)
+            {
+                MessageBox.Show("Health Insurance number entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_HealthInsuranceNumber.Focus();
+                return;
+            }
+            else if (!Regex.Match(textBox_PlaceOfBirth.Text, @"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$").Success)
+            {
+                MessageBox.Show("Place of birth cannot contain a digit!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox_PlaceOfBirth.Focus();
+                return;
+            }
+            else if(dateTimePicker_DateOfBirth.Value == DateTime.Now)
+            {
+                MessageBox.Show("Date of birth entered incorrectly!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dateTimePicker_DateOfBirth.Focus();
+                return;
+            }
+            Patient patient = new Patient();
+            patient.FirstName = textBox_FirstName.Text;
+            patient.LastName = textBox_LastName.Text;
+            patient.PersonalNumber = textBox_PersonalNumber.Text;
+            patient.HealthInsuranceNumber = textBox_HealthInsuranceNumber.Text;
+            patient.DateOfBirth= dateTimePicker_DateOfBirth.Value;
+            patient.PlaceOfBirth = textBox_PlaceOfBirth.Text;
+            patient.PhoneNumber= textBox_PhoneNumber.Text;
+            patient.Weight = textBox_Weight.Text;
+            patient.Height = textBox_Height.Text;
+            List<Patient> temp = doctorBusiness.GetAllPatients().Where(person => person.PersonalNumber == textBox_PersonalNumber.Text).ToList();
+            patient.Password = temp[0].Password;
+            patient.Email = temp[0].Email;
+            patient.Id = temp[0].Id;
+            string result = doctorBusiness.UpdatePatient(patient);
+            MessageBox.Show(result, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            dataGridView_PatientList.DataSource = doctorBusiness.GetAllPatients();
+            dataGridView_PatientList.Columns[7].Visible = false;
+            dataGridView_PatientList.Columns[8].Visible = false;
+        }
+
+        private void button_InsertPatient_Click(object sender, EventArgs e)
+        {
+            InsertPatientForm insertPatientForm = new InsertPatientForm(doctorBusiness);
+            insertPatientForm.RefPatientManagementForm = this;
+            insertPatientForm.FormClosed += new FormClosedEventHandler(FormClose);
+            insertPatientForm.Show();
+        }
+        private void FormClose(object sender, EventArgs e)
+        {
+            dataGridView_PatientList.DataSource = doctorBusiness.GetAllPatients();
+            dataGridView_PatientList.Columns[7].Visible = false;
+            dataGridView_PatientList.Columns[8].Visible = false;
+            this.Refresh();
         }
     }
 }
