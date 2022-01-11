ef35e3281c7f56a2f143f0f252967d99d88cbbcf
Bojana Bogicevic <bogicevicmbojana@gmail.com>
2022-01-11 15:31:55 +0100

    Add initial Insert Patient Form

---
+++ SI2021-05-MyClinic/PresentationLayerDesktop/InsertPatientForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace PresentationLayerDesktop
+{
+    partial class InsertPatientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InsertPatientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 479);
+            this.Name = "InsertPatientForm";
+            this.Text = "Insert Patient";
+            this.Load += new System.EventHandler(this.InsertPatientForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ SI2021-05-MyClinic/PresentationLayerDesktop/InsertPatientForm.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayerDesktop
+{
+    public partial class InsertPatientForm : Form
+    {
+        IDoctorBusiness doctorBusiness;
+        public InsertPatientForm(IDoctorBusiness _doctorBusiness)
+        {
+            doctorBusiness = _doctorBusiness;
+            InitializeComponent();
+        }
+        public PatientManagementForm RefPatientManagementForm { get; set; }
+        private void InsertPatientForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
