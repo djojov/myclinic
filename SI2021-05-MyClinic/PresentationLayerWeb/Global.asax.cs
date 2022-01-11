eaafddd2d0bb68da5cca3ebd22ac4eda1fe98873
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2022-01-11 15:55:25 +0100

    Add dependency injection

--- SI2021-05-MyClinic/PresentationLayerWeb/Global.asax.cs
+++ SI2021-05-MyClinic/PresentationLayerWeb/Global.asax.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer;
+using DataLayer;
+using DataLayer.Repositories;
+using Microsoft.AspNet.WebFormsDependencyInjection.Unity;
+using Shared.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +11,7 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using Unity;
 
 namespace PresentationLayerWeb
 {
@@ -13,9 +19,18 @@ namespace PresentationLayerWeb
     {
         void Application_Start(object sender, EventArgs e)
         {
+            var container = this.AddUnity();
+
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            container.RegisterType<IAdminRepository, AdminRepository>();
+            container.RegisterType<IAdminBusiness, AdminBusiness>();
+            container.RegisterType<IDoctorRepository, DoctorRepository>();
+            container.RegisterType<IDoctorBusiness, DoctorBusiness>();
+            container.RegisterType<IPatientRepository, PatientRepository>();
+            container.RegisterType<IPatientBusiness, PatientBusiness>();
         }
     }
 }
