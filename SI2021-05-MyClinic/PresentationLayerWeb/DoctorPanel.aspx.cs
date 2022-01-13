8d94fc08169f51d15662bf1d0aad864b738c9c0d
Djordje Jovanovic <djordje.jovanoviic@gmail.com>
2022-01-13 01:03:17 +0100

    Add functionality to fill doctor info on load

--- SI2021-05-MyClinic/PresentationLayerWeb/DoctorPanel.aspx.cs
+++ SI2021-05-MyClinic/PresentationLayerWeb/DoctorPanel.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shared.Interfaces;
+using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,40 @@ namespace PresentationLayerWeb
 {
     public partial class DoctorPanel : System.Web.UI.Page
     {
+        private IAdminBusiness adminBusiness;
+        private IDoctorBusiness doctorBusiness;
+        private IPatientBusiness patientBusiness;
+        public DoctorPanel() { }
+        public DoctorPanel(IAdminBusiness _adminBusiness)
+        {
+            adminBusiness = _adminBusiness;
+        }
+        public DoctorPanel(IDoctorBusiness _doctorBusiness)
+        {
+            doctorBusiness = _doctorBusiness;
+        }
+        public DoctorPanel(IPatientBusiness _patientBusiness)
+        {
+            patientBusiness = _patientBusiness;
+        }
+        public DoctorPanel(IAdminBusiness _adminBusiness, IDoctorBusiness _doctorBusiness, IPatientBusiness _patientBusiness)
+        {
+            adminBusiness = _adminBusiness;
+            doctorBusiness = _doctorBusiness;
+            patientBusiness = _patientBusiness;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            var doctor = (Doctor)Session["Doctor"];
+            TextBox_FirstName.Text = doctor.FirstName;
+            TextBox_LastName.Text = doctor.LastName;
+            TextBox_PersonalNumber.Text = doctor.PersonalNumber;
+            TextBox_PhoneNumber.Text = doctor.PhoneNumber;
+            TextBox_DateEmployed.Text = doctor.DateEmployed.ToString();
+            TextBox_Specialization.Text = doctor.Specialization;
+            TextBox_Department.Text = doctor.Department;
+            TextBox_Email.Text = doctor.Email;
         }
 
         protected void Button_PatientManagement_Click(object sender, EventArgs e)
