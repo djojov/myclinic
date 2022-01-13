323a12ce1d1cae7a91a43894326e37a0fbe5d396
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-13 16:52:52 +0100

    Setup PatientRepositoryTests

---
+++ SI2021-05-MyClinic/PatientRepositoryTests/PatientRepositoryTests.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Interfaces;
+using Shared.Models;
+using System;
+
+namespace PatientRepositoryTests
+{
+    [TestClass]
+    public class PatientRepositoryTests
+    {
+        public Doctor doctor;
+        public IDoctorRepository doctorRepository;
+        public Patient patient;
+        public IPatientRepository patientRepository;
+        [TestInitialize]
+        public void init() 
+        {
+            patient = new Patient
+            {
+                Id = 999999,
+                FirstName = "Boban",
+                LastName = "Bobanovic",
+                PersonalNumber = "2222222222222",
+                HealthInsuranceNumber = "22222222222",
+                DateOfBirth = Convert.ToDateTime("1975-01-01"),
+                PlaceOfBirth = "Boljevac",
+                Email = "somemail@gmail.com",
+                Password = "Password123@",
+                PhoneNumber = "0699999999",
+                Weight = "100",
+                Height = "190"
+            };
+            patientRepository = new PatientRepository();
+            doctorRepository = new DoctorRepository();
+        }
+       
+    }
+}
