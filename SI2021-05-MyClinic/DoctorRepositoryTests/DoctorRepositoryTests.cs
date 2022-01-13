291f85b33d166974dee88cf07fb87d7439b036d2
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-13 14:54:06 +0100

    Setup DoctorRepositoryTests

--- SI2021-05-MyClinic/DoctorRepositoryTests/DoctorRepositoryTests.cs
+++ SI2021-05-MyClinic/DoctorRepositoryTests/DoctorRepositoryTests.cs
@@ -1,14 +1,54 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DataLayer.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Interfaces;
+using Shared.Models;
 using System;
 
 namespace DoctorRepositoryTests
 {
     [TestClass]
-    public class UnitTest1
+    public class DoctorRepositoryTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public Doctor doctor;
+        public IDoctorRepository doctorRepository;
+
+        public Patient patient;
+        public IPatientRepository patientRepository;
+        [TestInitialize]
+        public void init()
         {
+            doctor = new Doctor
+            {
+                Id = 99999,
+                Status = true,
+                FirstName = "Goran",
+                LastName = "Goranovic",
+                PersonalNumber = "0000000000000",
+                PhoneNumber = "0600000000",
+                DateEmployed = Convert.ToDateTime("1975-01-01"),
+                Specialization = "psychology",
+                Department = "psychiatry",
+                Email = "xxxxxx@gmail.com",
+                Password = "Password123@"
+            };
+            patient = new Patient
+            {
+                Id = 99999999,
+                FirstName = "Boban",
+                LastName = "Bobanovic",
+                PersonalNumber = "1111111111111",
+                HealthInsuranceNumber = "22222222222",
+                DateOfBirth = Convert.ToDateTime("1975-01-01"),
+                PlaceOfBirth = "Boljevac",
+                Email="somemail@gmail.com",
+                Password="Password123@",
+                PhoneNumber="0699999999",
+                Weight="100",
+                Height="190"
+            };
+            doctorRepository = new DoctorRepository();
+            patientRepository = new PatientRepository();
         }
     }
 }
+
