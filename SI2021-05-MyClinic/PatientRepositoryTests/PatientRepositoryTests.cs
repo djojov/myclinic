14cd7e92d951dde8bcda05898ec72ba66c023740
Veljko Vulic <veljkovulice14@gmail.com>
2022-01-13 17:14:04 +0100

    Add GetPatientTest method to PatientRepositoryTests

--- SI2021-05-MyClinic/PatientRepositoryTests/PatientRepositoryTests.cs
+++ SI2021-05-MyClinic/PatientRepositoryTests/PatientRepositoryTests.cs
@@ -34,6 +34,22 @@ namespace PatientRepositoryTests
             patientRepository = new PatientRepository();
             doctorRepository = new DoctorRepository();
         }
-       
+        [TestMethod]
+        public void GetPatientTest()
+        {
+            Patient patient = patientRepository.GetPatient("urosmilosevic@myclinic.com", "Password123@");
+            Assert.AreEqual(patient.Id, patient.Id);
+            Assert.AreEqual(patient.FirstName, patient.FirstName);
+            Assert.AreEqual(patient.LastName, patient.LastName);
+            Assert.AreEqual(patient.PersonalNumber, patient.PersonalNumber);
+            Assert.AreEqual(patient.HealthInsuranceNumber, patient.HealthInsuranceNumber);
+            Assert.AreEqual(patient.DateOfBirth, patient.DateOfBirth);
+            Assert.AreEqual(patient.PlaceOfBirth, patient.PlaceOfBirth);
+            Assert.AreEqual(patient.Email, patient.Email);
+            Assert.AreEqual(patient.Password, patient.Password);
+            Assert.AreEqual(patient.PhoneNumber, patient.PhoneNumber);
+            Assert.AreEqual(patient.Weight, patient.Weight);
+            Assert.AreEqual(patient.Height, patient.Height);
+        }
     }
 }
